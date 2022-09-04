72ece1341bd03976988079a191f678a9de05d43a
Jorge Gonzaga <jsgonzaga@uol.com.br>
2022-09-04 18:30:13 -0300

    Update

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useTransactions } from "../../hooks/useTransactions";
 import { Container } from "./styles";
 
-interface Transaction {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
-}
-
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  useEffect(() => {
-    api.get('transactions')
-       .then(response => setTransactions(response.data.transactions))
-  }, []);
+  const { transactions } = useTransactions();
+ 
   return (
     <Container>
       <table>
@@ -31,7 +17,7 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          {transactions.map(transaction => (
+          {transactions?.map(transaction => (
               <tr key={transaction.id}>
                 <td>{transaction.title}</td>
                 <td className={transaction.type}>
